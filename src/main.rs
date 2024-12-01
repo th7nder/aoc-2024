1ee0b7a850b8a429371e6d78d53345cdbe946c3d
Konrad Stepniak <konrad@stepniak.net>
2024-12-01 12:32:59 +0100

    day1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{fs::File, io::{self, BufRead}, path::Path};
+
+mod day1;
+
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
     println!("Hello, world!");
 }
