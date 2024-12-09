57a0960b44374bf9f8889f509a8f31a3e0856c25
konrad.stepniak <konrad@eiger.co>
2024-12-09 16:15:17 +0100

    day 9

--- src/day09.rs
+++ src/day09.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day9)]
@@ -70,12 +72,64 @@ pub fn part1(sizes: &Vec<u8>) -> u64 {
 
 #[aoc(day9, part2)]
 pub fn part2(sizes: &Vec<u8>) -> u64 {
-    let mut blocks = create_blocks(sizes);
+    let mut id: i64 = 0;
 
-    
+    let mut files = HashMap::new();
+    let mut blanks = Vec::new();
 
+    let mut pos: usize = 0;
+    for (i, size) in sizes.into_iter().enumerate() {
+        if i % 2 == 0 {
+            if *size == 0 {
+                panic!("i don't know how to handle those files")
+            }
+            files.insert(id, (pos, *size));
+            id += 1;
+        } else if *size != 0 {
+            blanks.push((pos, *size));
+        }
 
-    checksum(&blocks)
+        pos += *size as usize;
+    }
+
+    while id > 0 {
+        id -= 1;
+        let (pos, size) = files[&id];
+
+        // println!("Checking {}", id);
+
+        let num_blanks = blanks.len();
+        for i in 0..num_blanks {
+            let (start, length) = blanks[i];
+            if start >= pos {
+                // blanks.truncate(i);
+                break
+            }
+            if size <= length {
+                files.insert(id, (start, size));
+                // println!("Inserting: {}, at {}, {}", id, start, size);
+
+                if size == length {
+                    blanks.remove(i);
+                } else {
+                    blanks[i] = (start + size as usize, length - size);
+                }
+                break;
+            }   
+        }
+    }
+
+    let mut checksum = 0;
+    // println!("{:?}", files);
+    for (fid, (pos, size)) in files.into_iter() {
+
+        for x in pos..(pos + size as usize) {
+            checksum += (fid as usize) * x;
+        }
+
+    }
+    // q
+    checksum as u64
 }
 
 
@@ -83,11 +137,17 @@ pub fn part2(sizes: &Vec<u8>) -> u64 {
 mod tests {
     use super::*;
 
-    static TEST_INPUT: &str = r"23331331214141314025";
+    static TEST_INPUT: &str = r"2333133121414131402";
     
     #[test]
     fn part1_example() {
         assert_eq!(part1(&parse_input(TEST_INPUT)), 1928);
     }
 
+
+        
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse_input(TEST_INPUT)), 2858);
+    }
 }
